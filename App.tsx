329cde4302c18040a5d1bcd4ab67a9c3c7093ea9
Vini Fraga <vinifragam@gmail.com>
2022-08-01 21:59:21 -0300

    Fundamentos - Estilizando Elementos

--- App.tsx
+++ App.tsx
@@ -2,9 +2,26 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text key="1">React</Text>
-      <Text key="2">Native</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+          color: '#FDFCFE',
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginTop: 48
+        }}>
+        Nome do evento
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B',
+        fontSize: 16
+      }}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
     </View>
   )
 }

275a7ddf0c0386195b8c03811c23125177fb0fe2
Vini Fraga <vinifragam@gmail.com>
2022-08-02 22:57:46 -0300

    Finalizando o App - Removendo participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -22,7 +22,7 @@ export function Home() {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert("Deletado!")
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: 'Não',

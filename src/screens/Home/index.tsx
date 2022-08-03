dc5937d459d79798a64ed4425d4a026b465774b3
Vini Fraga <vinifragam@gmail.com>
2022-08-02 22:41:06 -0300

    Finalizando o App - Adicionando Participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { Participant } from "../components/Participant";
@@ -5,12 +6,14 @@ import { Participant } from "../components/Participant";
 import { styles } from "./style";
 
 export function Home() {
-  const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João'];
+  const [participants, setParticipants] = useState(['João']);
   
   function handleParticipantAdd() {
     if (participants.includes("Rodrigo")) {
       return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
     }
+
+    setParticipants(prevState => [...prevState, 'Ana']);
   }
 
   function handleParticipantRemove(name: string) {

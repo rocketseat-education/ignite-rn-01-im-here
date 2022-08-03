e509d7ffa22e7da8dd011c74d2c4393fbbed862d
Vini Fraga <vinifragam@gmail.com>
2022-08-02 21:00:30 -0300

    Fundamentos - Estilizando a StatusBar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
+import { StatusBar } from "react-native";
+
 import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent" 
+        translucent
+      />
+      <Home />
+    </>
   )
 }
